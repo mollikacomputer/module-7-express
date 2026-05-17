bf74e2f859258ff7643430155eade0e6adcf6dc9
mollikacomputer <mollikacomputer3@gmail.com>
2026-05-17 09:24:55 +0600

    postgrase implement

---
+++ src/server.ts
@@ -0,0 +1,77 @@
+import express, { type Application, type Request, type Response } from 'express'
+import {Pool} from 'pg'
+const app :Application= express();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended:true}));
+
+const pool = new Pool({
+  connectionString:"postgresql://neondb_owner:npg_s5fkG7AoNBVY@ep-quiet-surf-ap15ybnl-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+});
+
+
+
+const initDB = async ()=>{
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `)
+      console.log("database connect successfully!")
+  } catch (error) {
+    console.log(error)
+  }
+};
+initDB();
+
+// const initDB = async ()=>{
+//   try {
+//     await pool.query(`
+//       // write query here
+//       `)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+app.get('/', (req : Request, res : Response) => {
+  res.send({'message': 'Express server', 'Author':'Mollika Computer'})
+});
+
+//2 post
+app.post('/', async(req:Request, res: Response)=>{
+  // console.log(req.body)
+  // const body = req.body;
+  const {name, email, password, age} = req.body;
+  const result = await pool.query(`
+    INSERT INTO users(name, email, password, age) VALUES($1,$2,$3,$4) RETURNING *
+    `, [name, email, password, age])
+    console.log(result)
+  res.status(201).json({
+    message:"Created",
+    data:{
+      name,
+      email,
+      password,
+      age,
+    }
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+// 7.4 start
+
+// postgresql://neondb_owner:npg_s5fkG7AoNBVY@ep-quiet-surf-ap15ybnl.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require
